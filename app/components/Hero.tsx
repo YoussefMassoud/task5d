4189fa8f53a99d6fa108c82a5f4ae001b0b6b332
YoussefMassoud <youssefbusiness133@gmail.com>
2024-06-21 05:22:31 +0300

    finsh hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <div className="flex justify-between items-start">
+      <div className="flex flex-col py-28 items-start px-40">
+        <div className="text-7xl font-medium">
+          Hi, I am <br /> 
+          Hassan 🎨
+        </div>
+        <div className="text-sm font-medium mt-4">
+          I am a UI/UX Designer, I like to make interfaces simple<br />
+          and aesthetically pleasing for users; The idea is not to <br />
+          create an interface for creating it, it is that users <br />
+          prefer you because your product is easy to use.<br />
+        </div>
+      </div>
+      
+      <div className="flex justify-end px-28">
+        <Image src="/heronextimages.png" alt="hero" width={800} height={800} />
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Nav from "./components/nav";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
   <div >
-    <Nav />
+    
+    <Nav /><Hero />
   </div>
   );
 }
